1d70366bafc6df855a154ec4100255e72b616e97
hemgom <reborn.april23@gmail.com>
2024-04-10 00:58:55 +0900

    studied step.01-06-01~04

---
+++ step.01_Java ORM Standard JPA Programming - Basic/section.06_various relationship mapping/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -0,0 +1,58 @@
+package hellojpa;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String name;
+    private int age;
+
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+}
